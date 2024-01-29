bug in reward calculation

configs/imagination_trainer.py
@@ -352,7 +352,7 @@ class QObserver(basics.ActorObserver):
     actions = jnp.stack(self.actions)[:npreds]
     q_values = rlax.batched_index(q_values, actions)
 
-    rewards = jnp.stack([t.reward for t in self.timesteps])[1:]
+    rewards = jnp.stack([t.reward for t in self.timesteps[1:]])
     # Create a figure and axis
     fig, ax = plt.subplots()
     ax.plot(q_values, label='q_values')